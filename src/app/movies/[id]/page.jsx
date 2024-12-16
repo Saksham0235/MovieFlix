9fe68da4d78ad4cff09f3672d0b67eae19db582a
saksham <ssharma1@univoedu.com>
2024-12-16 14:52:10 +0530

    fixing bugs

--- src/app/movies/[id]/page.jsx
+++ src/app/movies/[id]/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState, use } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "@/app/styles/common.module.css";
 import Image from "next/image";
 import Loading from "@/app/Loading";
