4e98c36d6123edf8d232442e2897575d81bd51a7
Saksham <ssharma1@univoedu.com>
2026-04-10 00:10:24 +0530

    Intersection Observer Implemented

--- src/app/movies/page.jsx
+++ src/app/movies/page.jsx
@@ -1,19 +1,14 @@
 "use client";
 import React, { use, useEffect, useMemo, useRef, useState } from "react";
-import { Suspense } from "react";
 import MovieCard from "@/app/components/MovieCard";
 import styles from "@/app/styles/common.module.css";
-import Link from "next/link";
 import { DebounceSearch, rawData } from "./[id]/utils";
-import { useSearchParams } from "next/navigation";
 
 const Movie = () => {
-  const searchParams = useSearchParams();
-  const ref=useRef(null)
-  const pageParam = searchParams.get("page");
+  const ref = useRef(null);
   const [input, setInput] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
-  const [data,setData]=useState([])
+  const [slide, setSlides] = useState(8);
   const debouncedSearch = useMemo(() => {
     return DebounceSearch(setSearchTerm, 500);
   }, []);
@@ -24,65 +19,38 @@ const Movie = () => {
   };
   useEffect(() => {
     return () => {
-      debouncedSearch.cancel(); // cleanup
+      debouncedSearch.cancel(); 
     };
   }, [debouncedSearch]);
 
-  const totalData = 177;
-  const dataPerPage = 8;
+  const main_data = rawData?.titles;
+  const filteredData = main_data?.filter((ele) =>ele?.jawSummary?.title?.substring(0, 18)?.toLowerCase()?.includes(searchTerm.toLowerCase())).slice(0,slide)
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const target = entries[0];
 
-  const totalPages = Math.ceil(totalData / dataPerPage);
-  // Creating offset i.e . the number of items to skip before beginning to collect the result set
-
-  let currPage = 1;
-
-  if (Number(pageParam) >= 1) {
-    currPage = Number(pageParam);
-  }
-
-  // We have to leave these offset before showing the next data
-  let offset = (currPage - 1) * dataPerPage;
-  let pageNumbers = [];
-
-  //  Creating loop to generate and show pagenumbers before and after the curr page
-  for (let i = currPage - 3; i <= currPage + 3; i++) {
-    if (i < 1) continue;
-    if (i > totalPages) break;
-
-    pageNumbers.push(i);
-  }
-
-  const options = {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": "ff6a4736b9msh38e290b88238d8fp108eb8jsnd3124f857ec2",
-      "x-rapidapi-host": "netflix54.p.rapidapi.com",
-    },
-  };
-
-
-  useEffect(()=>{
-    const getData = async () => {
-      try {
-        const res = await fetch(`https://netflix54.p.rapidapi.com/search/?query=stranger&offset=${offset}&limit_titles=${dataPerPage}&limit_suggestions=20&lang=en`,
-          options
-        );
-        const data = await res.json();
-        console.log(JSON.stringify(data), "from data");
-        setData(data?.titles)
-      } catch (err) {
-        console.log("Error in FEtching DAta -> ", err);
+        if (target.isIntersecting) {
+          setSlides((prev) => {
+            if (prev >= (main_data?.length || 0)) return prev;
+            return prev + 4;
+          });
+        }
+      },
+      {
+        root: null,
+        rootMargin: "100px",
+        threshold: 0,
       }
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      if (ref.current) observer.unobserve(ref.current);
     };
-    getData()
-  },[])
+  }, []);
 
-  
-  const main_data = data;
-  console.log(main_data,'from main_data')
-  const filteredData = main_data?.filter((ele) =>
-    ele?.jawSummary?.title?.substring(0, 18)?.toLowerCase()?.includes(searchTerm.toLowerCase())
-  );
 
   return (
     <>
@@ -91,7 +59,12 @@ const Movie = () => {
           <div className={styles.headerContainer}>
             <h1>Series & Movie</h1>
             <div className={styles.searchContainer}>
-              <input placeholder="Search... " className={styles.inputbox} value={input} onChange={handleInput} />
+              <input
+                placeholder="Search... "
+                className={styles.inputbox}
+                value={input}
+                onChange={handleInput}
+              />
               <img
                 className={styles.searchicon}
                 src="/searchicon.png"
@@ -100,41 +73,16 @@ const Movie = () => {
             </div>
           </div>
           <div className={styles.card_section}>
-            {filteredData.map((curElem,ind) => {
+            {filteredData.map((curElem, ind) => {
               return <MovieCard key={ind} {...curElem} />;
             })}
           </div>
         </div>
-
-        <div
-          style={{
-            display: "flex",
-            gap: "2rem",
-            marginTop: "12rem",
-            justifyContent: "center",
-          }}
-        >
-          {currPage - 1 >= 1 && (
-            <>
-              <Link href="/movies">{"<<"}</Link>
-            </>
-          )}
-          {pageNumbers.map((ele) => (
-            <Link
-              key={ele}
-              href={`/movies?page=${ele}`}
-              className={ele === currPage ? styles.activeLink : ""}
-            >
-              {ele}
-            </Link>
-          ))}
-
-          {currPage + 1 <= totalPages && (
-            <>
-              <Link href="/movies">{">>"}</Link>
-            </>
-          )}
-        </div>
+        { slide < main_data.length &&
+          <div ref={ref} className={styles.loadingState}>
+            Loading...
+          </div>
+        }
       </section>
     </>
   );
